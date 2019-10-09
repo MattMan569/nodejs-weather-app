6fe5a5822d6ca6e2a4e2857c972709b0abda4946
MattMan569 <matthewpolsom@hotmail.com>
2019-10-09 13:11:42 -0600

    Setup basic weather request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,8 @@
 const request = require('request');
 
+const url = `https://api.darksky.net/forecast/${process.env.DarkSkyApiKey}/37.8267,-122.4233?units=ca`;
 
+request({url: url, json: true}, (error, response) => {
+    const currently = response.body.currently;
+    console.log(`${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees. There is a ${currently.precipProbability}% chance of rain.`);
+});
