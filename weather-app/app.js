c5da3280089363b7ae5f5b0afce7757b8dee967f
MattMan569 <matthewpolsom@hotmail.com>
2019-10-15 17:39:11 -0600

    added basic weather and geocode calls

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,36 @@
 const request = require('request');
 
-const url = `https://api.darksky.net/forecast/${process.env.DarkSkyApiKey}/37.8267,-122.4233?units=ca`;
+const weatherUrl = `https://api.darksky.net/forecast/${process.env.DarkSkyApiKey}/37.8267,-122.4233?units=ca`;
 
-request({url: url, json: true}, (error, response) => {
+request({url: weatherUrl, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to weather service!');
+        return;
+    }
+
+    if (response.body.error) {
+        console.log('Unable to find location!');
+        return;
+    }
+
     const currently = response.body.currently;
     console.log(`${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees. There is a ${currently.precipProbability}% chance of rain.`);
+});
+
+const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MapBoxApiKey}&limit=1`;
+
+request({url: geocodeUrl, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location service!');
+        return;
+    }
+
+    if (response.body.features.length === 0) {
+        console.log('Unable to find location! Try again with a new search term.');
+        return;
+    }
+
+    const longitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    console.log(`${latitude}\n${longitude}`);
 });
